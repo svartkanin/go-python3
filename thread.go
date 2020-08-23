dc785dda71222ea52ae9c586000bf19f7e5d8671
Daniel Girtler <girtler.daniel@gmail.com>
2020-08-23 17:19:23 +1000

    Remove bindings for Python3.8 support

--- thread.go
+++ thread.go
@@ -48,11 +48,6 @@ func PyThreadState_Swap(tstate *PyThreadState) *PyThreadState {
 	return (*PyThreadState)(C.PyThreadState_Swap((*C.PyThreadState)(tstate)))
 }
 
-//PyEval_ReInitThreads : https://docs.python.org/3/c-api/init.html#c.PyEval_ReInitThreads
-func PyEval_ReInitThreads() {
-	C.PyEval_ReInitThreads()
-}
-
 //PyGILState_Ensure : https://docs.python.org/3/c-api/init.html#c.PyGILState_Ensure
 func PyGILState_Ensure() PyGILState {
 	return PyGILState(C.PyGILState_Ensure())
